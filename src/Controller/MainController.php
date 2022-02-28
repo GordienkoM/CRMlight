054bc9a484deeb497fd7a96c6792cdb967cec5b9
GordienkoM <mgordienkom@gmail.com>
2022-02-28 16:14:47 +0100

    fill template base and change asset

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/", name="dashboard")
+     * @Route("/", name="main")
      */
     public function index(): Response
     {
