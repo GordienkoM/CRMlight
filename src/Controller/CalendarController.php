fba2d1dda07a38d2a70f957277013fe70c0b486f
GordienkoM <mgordienkom@gmail.com>
2022-03-02 14:46:21 +0100

    display event

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -2,18 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EventRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CalendarController extends AbstractController
 {
     /**
      * @Route("/calendar", name="calendar")
      */
-    public function index(): Response
+    public function index(EventRepository $event): Response
     {
-        return $this->render('calendar/index.html.twig', [
-        ]);
+        $events = $event->findAll();
+
+        $rdvs = [];
+
+        foreach($events as $event){
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'borderColor' => $event->getBorderColor(),
+                'textColor' => $event->getTextColor(),
+                'allDay' => $event->getAllDay(),
+            ];
+        }
+
+        $data = json_encode($rdvs);
+
+        return $this->render('calendar/index.html.twig', compact('data'));
     }
 }
--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,8 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class EventType extends AbstractType
 {
