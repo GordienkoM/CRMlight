aa3149c902014c7f113a611923b8713539e0853d
GordienkoM <mgordienkom@gmail.com>
2022-03-06 23:52:23 +0100

    add conntact crud

---
+++ src/Form/ContactType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add('phone_number')
+            ->add('enable')
+            ->add('createdAt')
+            ->add('company')
+            ->add('categories')
+            ->add('creator')
+            ->add('events')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
