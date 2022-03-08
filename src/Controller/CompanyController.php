87db1a08273c386a072885ceaa87696e6b0dbcc5
GordienkoM <mgordienkom@gmail.com>
2022-03-08 10:17:39 +0100

    add Company

---
+++ src/Controller/CompanyController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Form\CompanyType;
+use App\Repository\CompanyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @Route("/company")
+ */
+class CompanyController extends AbstractController
+{
+    /**
+     * @Route("/", name="company_index", methods={"GET"})
+     */
+    public function index(CompanyRepository $companyRepository): Response
+    {
+        $companies = $companyRepository->findBy(
+            ['enable' => 'true'],
+            ['lastname' => 'ASC']
+        );
+
+        return $this->render('company/index.html.twig', [
+            'companies' => $companies,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="company_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $company = new Company();
+        // connected in user
+        $user = $this->getUser();
+        $company->setCreator($user);
+
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($company);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('company/new.html.twig', [
+            'company' => $company,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="company_show", methods={"GET"})
+     */
+    public function show(Company $company): Response
+    {
+        return $this->render('company/show.html.twig', [
+            'company' => $company,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="company_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Company $company, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $form = $this->createForm(CompanyType::class, $company);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('company/edit.html.twig', [
+            'company' => $company,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="company_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Company $company, ManagerRegistry $doctrine): Response
+    {
+        $entityManager = $doctrine->getManager();
+        
+        if ($this->isCsrfTokenValid('delete'.$company->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($company);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('company_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
--- src/Form/CompanyType.php
+++ src/Form/CompanyType.php
@@ -2,50 +2,55 @@
 
 namespace App\Form;
 
-use App\Entity\Contact;
+use App\Entity\Company;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class ContactType extends AbstractType
+class CompanyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname', TextType::class, [
-                'label_format' => 'Prénom',
+            ->add('name', TextType::class, [
+                'label_format' => 'Nom de tableau',
                 'attr' => ['class' => 'uk-input']
             ])
-            ->add('lastname', TextType::class, [
-                'label_format' => 'Nom',
+            ->add('address', TextType::class, [
+                'label_format' => 'Nom de tableau',
                 'attr' => ['class' => 'uk-input']
             ])
-            ->add('email', EmailType::class, [
-                "attr" => ['class' => 'uk-input'],
-                'label_format' => 'Email',
-            ])
-            ->add('phone_number', TextType::class, [
-                'label_format' => 'Numero de téléphone',
+            ->add('postalCode', TextType::class, [
+                'label_format' => 'Nom de tableau',
                 'attr' => ['class' => 'uk-input']
             ])
-
+            ->add('city', TextType::class, [
+                'label_format' => 'Nom de tableau',
+                'attr' => ['class' => 'uk-input']
+            ])
+            ->add('country', TextType::class, [
+                'label_format' => 'Nom de tableau',
+                'attr' => ['class' => 'uk-input']
+            ])
+            ->add('website', TextType::class, [
+                'label_format' => 'Nom de tableau',
+                'attr' => ['class' => 'uk-input']
+            ])
+            // ->add('enable')
+            // ->add('creator')
             ->add('submit', SubmitType::class, [
                 'label_format' => 'Ajouter',
                 'attr' => ['class' => 'uk-button uk-button-secondary uk-margin-top']
             ]);
-
-        //     ->add('company')
-        //     ->add('categories')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Contact::class,
+            'data_class' => Company::class,
         ]);
     }
 }
