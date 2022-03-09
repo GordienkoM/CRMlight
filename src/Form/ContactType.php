c62f2ae720c10d3d715290ed9a1716a1c253a314
GordienkoM <mgordienkom@gmail.com>
2022-03-09 03:18:04 +0100

    import class

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -6,8 +6,10 @@ use App\Entity\Contact;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class ContactType extends AbstractType
 {
